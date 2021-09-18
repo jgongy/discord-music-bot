145135c3edec24ce2c04cdf3f2877c9f655a9d5d
JackCHG <gongjackiegong@gmail.com>
2021-09-18 02:33:02 -0700

    Started implementing youtube dl in bot.py

--- bot.py
+++ bot.py
@@ -6,11 +6,60 @@ https://realpython.com/how-to-make-a-discord-bot-python/#what-is-a-bot
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+D_TOKEN = os.getenv("DISCORD_TOKEN")
+
+
 import discord
 from discord.ext import commands
 
-load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
+import youtube_dl
+import asyncio
+"""
+Streaming from Youtube using Youtube DL extracted from
+https://github.com/Rapptz/discord.py/blob/master/examples/basic_voice.py
+"""
+ytdl_format_options = {
+    "format": "bestaudio/best",
+    "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
+    "restrictfilenames": True,
+    "noplaylist": True,
+    "nocheckcertificate": True,
+    "ignoreerrors": False,
+    "logtostderr": False,
+    "quiet": True,
+    "no_warnings": True,
+    "default_search": "auto",
+    "source_address": "0.0.0.0" # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    "options": "-vn"
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+        self.title = data.get("title")
+        self.url = data.get("url")
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=True):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None,
+                                          lambda: ytdl.extract_info(url,
+                                                                    download = not stream))
+
+        if "entries" in data:
+            # take first item from a playlist
+            data = data["entries"][0]
+
+        filename = data["url"] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options),
+                   data=data)
 
 bot = commands.Bot(command_prefix="-")
 
@@ -26,7 +75,7 @@ async def play(context, *args):
         return
     # Message was sent by a valid user.
     
-    user = message.author#.voice.voice_channel
+    user = message.author
     voice_state = user.voice
     
     # Bot will join the user's voice channel, if possible
@@ -50,4 +99,4 @@ async def stop(context):
     await voice_client.disconnect()
     
     
-bot.run(TOKEN)
+bot.run(D_TOKEN)
